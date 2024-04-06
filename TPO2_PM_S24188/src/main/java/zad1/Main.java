4df78a3029eeb8cb002af22e0552703b8774e1c7
s24188 <s24188@pjwstk.edu.pl>
2024-04-06 14:41:50 +0200

    Logic implemented

--- TPO2_PM_S24188/src/main/java/zad1/Main.java
+++ TPO2_PM_S24188/src/main/java/zad1/Main.java
@@ -9,8 +9,8 @@ package zad1;
 
 public class Main {
   public static void main(String[] args) {
-    Service s = new Service("Poland");
-    String weatherJson = s.getWeather("Warsaw");
+    Service s = new Service("United Kingdom");
+    String weatherJson = s.getWeather("London");
     Double rate1 = s.getRateFor("USD");
     Double rate2 = s.getNBPRate();
     // ...
--- TPO2_PM_S24188/src/main/java/zad1/Service.java
+++ TPO2_PM_S24188/src/main/java/zad1/Service.java
@@ -7,40 +7,98 @@
 package zad1;
 
 
-import com.github.cliftonlabs.json_simple.Jsonable;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.Writer;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Currency;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
-public class Service implements Jsonable {
+public class Service {
 
-    public Service() {
+    private final Locale locale;
+    private final Currency localCurrency;
+    private final String weatherApiKey = "7d5145dcf76f5b018c2ba8c35d355530";
+    private final String currencyApiKey = "c0232a488d2e6782e4781e6e";
+    private final String weatherApiUrlName = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s";
+    private final String locationApiUrlName = "http://api.openweathermap.org/geo/1.0/direct?q=%s,%s&limit=1&appid=%s";
+    private final String currencyApiUrlName = "https://v6.exchangerate-api.com/v6/%s/pair/%s/%s";
+    private final String nbpApiUrlName = "http://api.nbp.pl/api/exchangerates/rates/A/%s";
+
+
+    public Service(String countryName) {
+        locale = getCountry(countryName);
+        localCurrency = Currency.getInstance(locale);
     }
 
-    public Service(String poland) {
+    public String getWeather(String cityName) {
+        try {
+            //getting location lat and lon from API
+            URL locationApiUrl = new URL(String.format(locationApiUrlName, cityName, locale.getISO3Country(), weatherApiKey));
+            String locationString = readFromUrl(locationApiUrl);
+            JSONObject locationJSON = new JSONObject(locationString.substring(1, locationString.length()-1));
 
+            //getting weather data from API based on lat and lon
+            Double lat = Double.parseDouble(locationJSON.get("lat").toString());
+            Double lon = Double.parseDouble(locationJSON.get("lon").toString());
+            URL weatherApiUrl = new URL(String.format(weatherApiUrlName, String.format("%.2f", lat), String.format("%.2f", lon), weatherApiKey));
+
+            return readFromUrl(weatherApiUrl);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public String getWeather(String warsaw) {
-        return null;
-    }
+    public Double getRateFor(String currency) {
+        try {
+            URL  currencyApiUrl = new URL(String.format(currencyApiUrlName, currencyApiKey, localCurrency, currency));
+            JSONObject currencyRateJSON = new JSONObject(readFromUrl(currencyApiUrl));
 
-
-    public Double getRateFor(String usd) {
-        return null;
+            return currencyRateJSON.getDouble("conversion_rate");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Double getNBPRate() {
-        return null;
+        if(localCurrency.getCurrencyCode().equals("PLN"))
+            return 1.0;
+        else {
+            try {
+                URL nbpApiUrl = new URL(String.format(nbpApiUrlName, localCurrency));
+                JSONObject nbpRateJSON = new JSONObject(readFromUrl(nbpApiUrl));
+
+                return nbpRateJSON.getJSONArray("rates").getJSONObject(0).getDouble("mid");
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
-    @Override
-    public String toJson() {
-        return null;
+    public static String readFromUrl(URL url) {
+        try (InputStream input = url.openStream()) {
+            InputStreamReader isr = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(isr);
+            StringBuilder json = new StringBuilder();
+            int c;
+            while ((c = reader.read()) != -1) {
+                json.append((char) c);
+            }
+            return json.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @Override
-    public void toJson(Writer writer) throws IOException {
-
+    public static Locale getCountry(String countryName){
+        List<Locale> countries = List.of(Locale.getAvailableLocales());
+        Optional<Locale> receivedCountry = countries.stream().filter(country -> country.getDisplayCountry().equalsIgnoreCase(countryName)).findFirst();
+        return receivedCountry.orElse(null);
     }
 }
